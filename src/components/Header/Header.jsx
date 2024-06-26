c37890e5f9cb2679b8718b55db2f94e1908b8902
Danilo Azevedo <danilo.ann@gmail.com>
2024-06-25 21:30:01 -0300

    Estilização e  animações, alterações finais

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
-const Header = () => {
+const Header = ({ user }) => {
     return <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
-        <span className="text-gray-100">Olá, Usuário</span>
-        <h1 className="text-gray-100 text-xl">Alura Newletter</h1>
-        <ToggleTheme className="h-8 text-gray-100 hidden sm:block cursor-pointer" />
+        <span className="text-gray-100">Olá, { user?.name || "Usuário"}</span>
+        <h1>Alura Newletter</h1>
+        <ToggleTheme />
     </div>
 }
 
